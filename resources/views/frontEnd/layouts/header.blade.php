a56c9937f0118a3ad97757dab748014242b8b71e
nhintt <nhintt2411@gmail.com>
2020-10-29 17:02:13 +0700

    Change main color

--- resources/views/frontEnd/layouts/header.blade.php
+++ resources/views/frontEnd/layouts/header.blade.php
@@ -77,7 +77,7 @@
     <div class="header-bottom"><!--header-bottom-->
         <div class="container">
             <div class="row">
-                <div class="col-sm-9">
+                <div class="col-sm-8">
                     <div class="navbar-header">
                         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                             <span class="sr-only">Toggle navigation</span>
@@ -100,7 +100,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-sm-3">
+                <div class="col-sm-4">
                     {{-- <div class="search_box pull-right">
                         <input type="text" placeholder="Search"/>
                     </div> --}}
@@ -108,7 +108,7 @@
                         {{csrf_field()}}
                         <div class="search_box pull-right">
                             <input type="text" name="keywords_submit" placeholder="Tìm kiếm sản phẩm"/>
-                            <input type="submit" style="margin-top:0;color:#fff; width: 30%" name="search_items" class="btn btn-primary btn-sm" value="Tìm kiếm">
+                            <input type="submit" style="margin-top:0;color:#333; width: 30%; font-weight: 600" name="search_items" class="btn btn-primary btn-sm" value="Tìm kiếm">
                         </div>
                     </form>
                 </div>
