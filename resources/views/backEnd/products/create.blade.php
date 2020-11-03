e25e78dde8f86ce5ad406fca675c9bcc169d2640
thuynt <thuybinh11997@gmail.com>
2020-11-03 14:19:50 +0700

    fix category

--- resources/views/backEnd/products/create.blade.php
+++ resources/views/backEnd/products/create.blade.php
@@ -65,7 +65,7 @@
                     </div>
                     <div class="control-group">
                         <label for="price" class="control-label">Price</label>
-                        <div class="controls{{number_format($errors->has('price')?' has-error':'')}}">
+                        <div class="controls{{$errors->has('price')?' has-error':''}}">
                             <div class="input-prepend"> <span class="add-on">đ</span>
                                 <input type="number" name="price" id="price" class="" value="{{old('price')}}" title="" required="required">
                                 <span class="text-danger">{{$errors->first('price')}}</span>
